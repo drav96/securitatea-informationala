381b94aeacda7d36d8b00189a3dc06c0fc837634
Dragos Lupei <lupei.dragos@gmail.com>
2017-10-04 00:55:14 +0300

    added maximum times of interval

--- Lab1/client.js
+++ Lab1/client.js
@@ -1,27 +1,33 @@
 const net = require('net');
 const readline = require('readline');
+
 let client = new net.Socket();
+
 let interval;
+let patt = /^(?:y(?:es)?|1)$/i;
+
 client.connect(6000, '127.0.0.1', () => {
 	const rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout
 	});
 
-
-	rl.question('What do you want to send to the server? ', (message) => {
-		rl.question('Would you like to setInterval? ', (answerInterval) => {
-			var patt = /^(?:y(?:es)?|1)$/i;
-			var result = patt.test(answerInterval);
+	rl.question('What message do you want to send to the server? ', (message) => {
+		rl.question('Would you like to set an interval for messages? ', (answerInterval) => {
+			let result = patt.test(answerInterval);
 			if (result) {
-				rl.question('Which interval? ', (intervalNumber) => {
-					rl.close();
+				rl.question('Set interval time ? ', (intervalNumber) => {
 					interval = intervalNumber;
-					setInterval(function () {
-						client.write(message);
-					}, parseInt(intervalNumber));
+					rl.question('Maximum number of messages? ', (numberOfMessages) => {
+						rl.close();
 
-				});
+						setIntervalLimited(function () {
+							client.write(message);
+
+						}, parseInt(intervalNumber), parseInt(numberOfMessages))
+
+					});
+				})
 			} else {
 				client.write(message);
 			}
@@ -41,3 +47,11 @@ client.on('close', () => {
 });
 
 
+// Definition
+function setIntervalLimited(callback, interval, x) {
+
+	for (let i = 0; i < x; i++) {
+		setTimeout(callback, i * interval);
+	}
+
+}
