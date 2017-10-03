9813c210805a3b657e8f4b888aff5c036b6aec7d
Dragos Lupei <lupei.dragos@gmail.com>
2017-10-04 00:40:32 +0300

    client + server

---
+++ Lab1/client.js
@@ -0,0 +1,43 @@
+const net = require('net');
+const readline = require('readline');
+let client = new net.Socket();
+let interval;
+client.connect(6000, '127.0.0.1', () => {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+
+	rl.question('What do you want to send to the server? ', (message) => {
+		rl.question('Would you like to setInterval? ', (answerInterval) => {
+			if (answerInterval === "yes") {
+				rl.question('Which interval? ', (intervalNumber) => {
+					rl.close();
+					interval = intervalNumber;
+					setInterval(function () {
+						client.write(message);
+					}, parseInt(intervalNumber));
+
+				});
+
+			} else {
+				console.log('Connected');
+				client.write(message);
+			}
+
+		})
+	});
+
+});
+
+client.on('data', (data) => {
+	console.log('Received: ' + data);
+	// client.destroy(); // kill client after server's response
+});
+
+client.on('close', () => {
+	console.log('Connection closed');
+});
+
+
